5ad8ca2f486eac5488b22ceb8137018fc2c6e712
Comvertex <matteo@comvertex.digital>
2024-11-04 20:46:36 +0000

    live 3.7

--- dist/script.js
+++ dist/script.js
@@ -18,13 +18,40 @@ window.addEventListener('scroll', () => {
 document.addEventListener('DOMContentLoaded', function() {
     const consentBanner = document.getElementById('consent-banner');
     const acceptBtn = document.getElementById('accept-btn');
+    const refuseBtn = document.getElementById('refuse-btn');
 
+    // Check if consent has already been given or refused
     if (!localStorage.getItem('cookieConsent')) {
         consentBanner.style.display = 'flex';
     }
 
     acceptBtn.addEventListener('click', function() {
-        localStorage.setItem('cookieConsent', 'true');
+        localStorage.setItem('cookieConsent', 'accepted');
+        consentGranted();
+        consentBanner.style.display = 'none';
+    });
+
+    refuseBtn.addEventListener('click', function() {
+        localStorage.setItem('cookieConsent', 'refused');
+        consentRefused();
         consentBanner.style.display = 'none';
     });
 });
+
+function consentGranted() {
+    gtag('consent', 'update', {
+        'ad_storage': 'granted',
+        'ad_user_data': 'granted',
+        'ad_personalization': 'granted',
+        'analytics_storage': 'granted'
+    });
+}
+
+function consentRefused() {
+    gtag('consent', 'update', {
+        'ad_storage': 'denied',
+        'ad_user_data': 'denied',
+        'ad_personalization': 'denied',
+        'analytics_storage': 'denied'
+    });
+}
