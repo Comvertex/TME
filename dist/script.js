3e79affba889308768b72fa7272d9e0bad254d4d
Comvertex <matteo@comvertex.digital>
2024-11-14 17:26:38 +0000

    new tag manager setup6

--- dist/script.js
+++ dist/script.js
@@ -14,11 +14,11 @@ window.addEventListener('scroll', () => {
     lastScroll = currentScroll;
 });
 
-// Consent banner logic
 document.addEventListener('DOMContentLoaded', function() {
     const consentBanner = document.getElementById('consent-banner');
     const acceptBtn = document.getElementById('accept-btn');
     const refuseBtn = document.getElementById('refuse-btn');
+    const whatsappButton = document.querySelector('.whatsapp-float');
 
     const consentStatus = localStorage.getItem('cookieConsent');
     console.log('Consent Status:', consentStatus);
@@ -34,6 +34,7 @@ document.addEventListener('DOMContentLoaded', function() {
         console.log('Consent Accepted');
         consentGranted();
         consentBanner.style.display = 'none';
+        adjustWhatsAppPosition();
     });
 
     refuseBtn.addEventListener('click', function() {
@@ -41,7 +42,23 @@ document.addEventListener('DOMContentLoaded', function() {
         console.log('Consent Refused');
         consentRefused();
         consentBanner.style.display = 'none';
+        adjustWhatsAppPosition();
     });
+
+    function adjustWhatsAppPosition() {
+        if (consentBanner.style.display === 'none') {
+            whatsappButton.style.bottom = '20px';
+        } else {
+            const bannerHeight = consentBanner.offsetHeight;
+            whatsappButton.style.bottom = `${bannerHeight + 20}px`;
+        }
+    }
+
+    // Adjust position on load
+    adjustWhatsAppPosition();
+
+    // Adjust position on window resize
+    window.addEventListener('resize', adjustWhatsAppPosition);
 });
 
 function consentGranted() {
@@ -61,34 +78,3 @@ function consentRefused() {
         'analytics_storage': 'denied'
     });
 }
-
-document.addEventListener('DOMContentLoaded', function() {
-    const consentBanner = document.querySelector('.consent-banner');
-    const whatsappButton = document.querySelector('.whatsapp-float');
-
-    function adjustWhatsAppPosition() {
-        if (consentBanner.style.display === 'none') {
-            whatsappButton.style.bottom = '20px';
-        } else {
-            const bannerHeight = consentBanner.offsetHeight;
-            whatsappButton.style.bottom = `${bannerHeight + 20}px`;
-        }
-    }
-
-    // Adjust position on load
-    adjustWhatsAppPosition();
-
-    // Adjust position on window resize
-    window.addEventListener('resize', adjustWhatsAppPosition);
-
-    // Adjust position when consent is given or refuseds
-    document.getElementById('accept-btn').addEventListener('click', function() {
-        consentBanner.style.display = 'none';
-        adjustWhatsAppPosition();
-    });
-
-    document.getElementById('refuse-btn').addEventListener('click', function() {
-        consentBanner.style.display = 'none';
-        adjustWhatsAppPosition();
-    });
-});
